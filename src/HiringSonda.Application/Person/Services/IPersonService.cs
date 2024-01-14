1d1253e2b320c4c874e7a9927c0de9d7fa8af1ca
Leandro Kuranaga <lskuranaga@stefanini.com>
2024-01-14 15:24:27 -0300

    Arrumando estrutura das pastas

---
+++ src/HiringSonda.Application/Person/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using HiringSonda.Application.Person.Models.Response;
+using HiringSonda.Domain.UserAggregate;
+
+namespace HiringSonda.Application.Person.Services
+{
+    public interface IPersonService
+    {
+        public Task<IEnumerable<UserResponse>> GetAllUsers();
+        public Task<UserDomain> GetUser(int id);
+        public Task RegisterAddress(UserDomain user);
+        public Task<UserDomain> GetAddressById(int id);
+
+    }
+}
